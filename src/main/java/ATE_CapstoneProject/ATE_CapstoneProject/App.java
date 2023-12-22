addd86b092dac2faa5b7dd3729cfe39a2a68e67a
Malay <mca.malay@gmail.com>
2023-12-22 10:53:11 +0530

    initial commit

---
+++ src/main/java/ATE_CapstoneProject/ATE_CapstoneProject/App.java
@@ -0,0 +1,13 @@
+package ATE_CapstoneProject.ATE_CapstoneProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
